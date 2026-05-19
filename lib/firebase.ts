6ab7661b17cf9ace0fea1fc3522a9e4141c423c1
Tharusha_Perera <134818242+TharushaPerera27070@users.noreply.github.com>
2026-05-19 12:57:07 +0530

    feat: install firebase SDK and implement project initialization utility

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,20 @@
-// TODO: Initialize your new database connection here
-// Example: export const db = newDatabaseClient();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const app = null;
-export const db = null;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ST4CgsDdEpbElgQMa_s0zZCuCMWIPc8",
+  authDomain: "japan-basic-erp.firebaseapp.com",
+  projectId: "japan-basic-erp",
+  storageBucket: "japan-basic-erp.firebasestorage.app",
+  messagingSenderId: "878245982841",
+  appId: "1:878245982841:web:b4f277230cad64b71f9aa8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = null;
